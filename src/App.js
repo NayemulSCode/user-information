dc734c5426ab1aa35d956f8cc1db62e4c578515f
Nayemul <nayemulhasan2016@gmail.com>
2021-02-28 03:39:17 +0600

    simple way to fetch data and dispaly from api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import FontCart from './FontCart';
+
 
 function App() {
+    const [users, setUsers] = useState([]);
+    useEffect(() =>{
+      fetch('https://randomuser.me/api/?results=50')
+      .then(res => res.json())
+      .then(data => {
+          setUsers(data.results);
+          console.log(data);
+          const names = users.map(user => user.name.first)
+          console.log(names)
+        })
+        .catch(error => console.log(error))
+    },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+        {
+          users.map(user=>
+           <p>{user.name.first}</p> 
+          )
+        }
+      </div>
+        <button>Add user</button>
+        <FontCart />
       </header>
     </div>
   );
---
+++ src/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Users(props) {
+    
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Users;
